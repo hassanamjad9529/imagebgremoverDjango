4820948910830aa273ef4346b85c4e42706c2fc7
Hassan Amjad <hassanamjad9529@gmail.com>
2025-05-25 12:06:38 +0500

    h

--- imagebgremover/settings.py
+++ imagebgremover/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k#*$2+pgzrx2u4$48fy283tn(-j*^@3b8alp@304y=-@1tk9x4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ["*"]
 
 
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
+
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,5 @@ STATICFILES_DIRS = []
 # ...existing code...
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
